a402a6bcf177e3fc8a1f006d91cb03838ddf6433
Emmanuel Komba <kombae@gmatc.matc.edu>
2025-02-16 20:14:24 -0600

    Update the GraphQL query for index.js

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,6 +14,14 @@ const IndexPage = ({ data }) => (
         data.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+              <GatsbyImage
+                image={edge.node.hero.gatsbyImageData}
+              />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
           </li>
         ))
         
@@ -38,6 +47,18 @@ export const query = graphql`
           id
           title
           slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          hero {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
       }
     }
